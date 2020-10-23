9040334c1a63223285d3c918aa3a1506bfab86e6
Mauricio López <mauricio.lopez@umanizales.edu.co>
2020-10-22 22:23:33 -0500

    create authors

---
+++ Controllers/AuthorController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using net_migrations.Models;
+using net_migrations.Domain;
+using net_migrations.Infrastructure;
+
+namespace net_migrations.Controllers
+{
+    public class AuthorController : Controller
+    {
+        private net_migrationsDbContext _Net_migrationsDbContext;
+
+        public AuthorController(net_migrationsDbContext Net_migrationsDbContext)
+        {
+            _Net_migrationsDbContext = Net_migrationsDbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Author author)
+        {
+            _Net_migrationsDbContext.Authors.Add(author);
+            int count = _Net_migrationsDbContext.SaveChanges();
+            return View(author);
+        }
+    }
+}
---
+++ Controllers/BookController copy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using net_migrations.Models;
+using net_migrations.Domain;
+
+namespace net_migrations.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
